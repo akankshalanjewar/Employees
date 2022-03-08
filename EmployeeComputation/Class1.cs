1c4369fcf3539f466dc8a2d1158172e54aa2686a
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-08 22:24:06 +0530

    added[akanksha] presentabsent

---
+++ EmployeeComputation/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComputation
+{
+    public class Class1
+    {
+        int IS_PRSENT = 1;
+        int IS_ABSENT = 0;
+        Random random = new Random();
+        public void myMethod()
+        {
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_PRSENT)
+            {
+                Console.WriteLine("Employee Is Present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Absent");
+
+            }
+        }
+    }
+}
