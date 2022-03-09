2ce716644f814bcc9ead3b2ed6ea996167b76192
akankshalanjewar <akankshalanjewar02@gmail.com>
2022-03-09 09:51:10 +0530

    added[akanksha]workingHours

--- EmployeeComputation/Class1.cs
+++ EmployeeComputation/Class1.cs
@@ -11,8 +11,15 @@ namespace EmployeeComputation
         int IS_PRSENT = 1;
         int IS_ABSENT = 0; 
         int EMP_RATE_PER_HR = 20;
+        int NUM_OF_WORKING_DAY = 20;
+        int MAX_RATE_PER_HR = 10;
         int empHrs = 0;
-        int empWage = 0;
+        int empWages = 0;
+        int totalWorkingDays = 0;
+        int NUM_OR_WORKING_DAYS = 20;
+        int totalEmpHrs = 0;
+        int totalSalary = 0;
+        
 
         Random random = new Random();
         public void myMethod()
@@ -30,7 +37,7 @@ namespace EmployeeComputation
 
             }
         }
-        public void empWages()
+        public void empWage()
         { 
         
 
@@ -43,11 +50,75 @@ namespace EmployeeComputation
             {
                 empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HR;
+            empWages = empHrs * EMP_RATE_PER_HR;
 
-            Console.WriteLine("EmpWage : " + empWage);
+            Console.WriteLine("EmpWages : " + empWages);
 
-       }      
+       }    
+        public void WagesOfMonth()
+        {
+            for (int i = 0; i < NUM_OF_WORKING_DAY; i++)
+            {
+                Random random = new Random();
+                int randomcheck = random.Next(0, 3);
+                switch (randomcheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+
+                    case 2:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                empWages= (empHrs + EMP_RATE_PER_HR);
+                totalSalary = (totalSalary + empWages);
+
+
+
+
+            }
+            Console.WriteLine("Total Emp Wages:" + totalSalary);
+        }
+        public void workingHours()
+        {
+            while (totalEmpHrs <= MAX_RATE_PER_HR && totalWorkingDays <= NUM_OR_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int randomCheck = random.Next(0, 3);
+
+                switch (randomCheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+
+                    case 2:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "EmpHrs:" + empHrs);
+
+
+            }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("TotalEmpWage:" + totalEmpWage);
+
+
+        }
     }
-
 }
+    
+
